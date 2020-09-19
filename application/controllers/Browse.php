fb7141538e6715427ee36ebd3d266691b39008c8
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-19 11:10:25 -0400

    Add empty browse controller

---
+++ application/controllers/Browse.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Browse extends CI_Controller {
+
+	/**
+	 * Main browse page
+	 * Parameters are each part of the path
+	 */
+	public function index()
+	{
+		$path_array = func_get_args();
+		
+	}
+}
