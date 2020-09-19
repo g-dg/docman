222c870ffbbae187db9d14d1c86a4237a83ca889
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-19 11:10:36 -0400

    Add empty setup controller

---
+++ application/controllers/Setup.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Setup extends CI_Controller {
+
+	/**
+	 * Sets up the system according to the configuration
+	 */
+	public function index()
+	{
+		
+	}
+}
