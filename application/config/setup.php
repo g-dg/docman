cf1cb1206bf2005e0ee23f8906fd764de9a99484
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-19 11:10:03 -0400

    Add setup config file

---
+++ application/config/setup.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * These values are used during the initial setup
+ */
+$config['docman_default_username'] = 'admin';
+$config['docman_default_password'] = 'password';
