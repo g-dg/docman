a5a80175ffa4e027ab1fe1b8ffb1395ba10af5e9
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-22 15:11:15 -0400

    Use global CodeIgniter object in authentication library

--- application/libraries/Authentication.php
+++ application/libraries/Authentication.php
@@ -3,16 +3,16 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Authentication {
 
-	private $db;
+	private $CI;
 
 	const AUTH_SUCCESS = 1;
 	const AUTH_BAD_USERNAME = 2;
 	const AUTH_BAD_PASSWORD = 4;
 	const AUTH_NEED_PASSWORD_CHANGE = 8;
 
-	public function __construct($db)
+	public function __construct()
 	{
-		$this->db = $db;
+		$this->CI =& get_instance();
 	}
 
 	/**
