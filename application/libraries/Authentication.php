9fa7591e90ce582c61714a864d5792d4e4187b24
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-22 12:51:35 -0400

    Add authentication stub

---
+++ application/libraries/Authentication.php
@@ -0,0 +1,26 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Authentication {
+
+	private $db;
+
+	const AUTH_SUCCESS = 1;
+	const AUTH_BAD_USERNAME = 2;
+	const AUTH_BAD_PASSWORD = 4;
+	const AUTH_NEED_PASSWORD_CHANGE = 8;
+
+	public function __construct($db)
+	{
+		$this->db = $db;
+	}
+
+	/**
+	 * 
+	 */
+	public function authenticate($username, $password)
+	{
+
+	}
+
+}
