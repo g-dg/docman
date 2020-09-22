1d8d8e05c15a39edbced7107a1ea7246d69d39cd
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-22 12:42:26 -0400

    Add "IF NOT EXISTS" to database creation scripts

--- resources/database_schema.sql
+++ resources/database_schema.sql
@@ -8,10 +8,10 @@ BEGIN TRANSACTION;
 PRAGMA user_version = 1;
 
 -- CodeIgniter sessions table
-CREATE TABLE "ci_sessions"("id" TEXT PRIMARY KEY NOT NULL,"ip_address" TEXT NOT NULL,"timestamp" INTEGER DEFAULT 0 NOT NULL,"data" TEXT DEFAULT '' NOT NULL);CREATE INDEX "ci_sessions_timestamp" ON "ci_sessions"("timestamp");
+CREATE TABLE IF NOT EXISTS "ci_sessions"("id" TEXT PRIMARY KEY NOT NULL,"ip_address" TEXT NOT NULL,"timestamp" INTEGER DEFAULT 0 NOT NULL,"data" TEXT DEFAULT '' NOT NULL);CREATE INDEX IF NOT EXISTS "ci_sessions_timestamp" ON "ci_sessions"("timestamp");
 
 -- Stores users
-CREATE TABLE "users" (
+CREATE TABLE IF NOT EXISTS "users" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
 	"username" TEXT NOT NULL UNIQUE, -- username used to log in
 	"password" TEXT, -- hashed password
@@ -22,7 +22,7 @@ CREATE TABLE "users" (
 );
 
 -- Stores user logins
-CREATE TABLE "logins" (
+CREATE TABLE IF NOT EXISTS "logins" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
 	"user_id" INTEGER NOT NULL REFERENCES "users",
 	"client_addr" TEXT,
@@ -32,20 +32,20 @@ CREATE TABLE "logins" (
 );
 
 -- Stores groups
-CREATE TABLE "groups" (
+CREATE TABLE IF NOT EXISTS "groups" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
 	"name" TEXT NOT NULL UNIQUE
 );
 
 -- Maps users to groups
-CREATE TABLE "users_in_groups" (
+CREATE TABLE IF NOT EXISTS "users_in_groups" (
 	"user_id" INTEGER NOT NULL,
 	"group_id" INTEGER NOT NULL,
 	UNIQUE("user_id", "group_id")
 );
 
 -- This table describes which mountpoints exist and where they are hooked into the user-facing filesystem
-CREATE TABLE "mountpoints" (
+CREATE TABLE IF NOT EXISTS "mountpoints" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
 	"destination_path" TEXT NOT NULL UNIQUE, -- where in the user-facing filesystem the mountpoint is mounted
 	"driver" TEXT NOT NULL, -- which fs driver to use
@@ -53,7 +53,7 @@ CREATE TABLE "mountpoints" (
 );
 
 -- This table is abstracted through the fs driver and doesn't have to be used since files shall not be refered to by their numeric id outside of the driver.
-CREATE TABLE "files" (
+CREATE TABLE IF NOT EXISTS "files" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
 	"mountpoint_id" INTEGER NOT NULL REFERENCES "mountpoints", -- which mountpoint this file is part of
 	"path_in_mountpoint" TEXT NOT NULL, -- path inside of the mountpoint
@@ -64,7 +64,7 @@ CREATE TABLE "files" (
 );
 
 -- Defines the permissions on a file for a group. Abstracted through the fs driver
-CREATE TABLE "file_permissions" (
+CREATE TABLE IF NOT EXISTS "file_permissions" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
 	"file_id" INTEGER NOT NULL REFERENCES "files",
 	"group_id" INTEGER NOT NULL REFERENCES "groups", -- the group that has access set
@@ -76,21 +76,21 @@ CREATE TABLE "file_permissions" (
 );
 
 -- Contains the available tags
-CREATE TABLE "tags" (
+CREATE TABLE IF NOT EXISTS "tags" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
 	"name" TEXT NOT NULL UNIQUE,
 	"description" TEXT
 );
 
 -- Describes the tags on each file, managed by the fs driver
-CREATE TABLE "tagged_files" (
+CREATE TABLE IF NOT EXISTS "tagged_files" (
 	"file_id" INTEGER NOT NULL REFERENCES "files",
 	"tag_id" INTEGER NOT NULL REFERENCES "tags",
 	PRIMARY KEY("file_id", "tag_id")
 );
 
 -- Stores the file history of users
-CREATE TABLE "file_history" (
+CREATE TABLE IF NOT EXISTS "file_history" (
 	"id" INTEGER PRIMARY KEY,
 	"user_id" INTEGER NOT NULL REFERENCES "users",
 	"file_path" TEXT NOT NULL,
@@ -98,7 +98,7 @@ CREATE TABLE "file_history" (
 	"action" TEXT NOT NULL
 );
 
-CREATE TABLE "favourites" (
+CREATE TABLE IF NOT EXISTS "favourites" (
 	"id" INTEGER PRIMARY KEY,
 	"user_id" INTEGER NOT NULL REFERENCES "users",
 	"file_path" TEXT NOT NULL,
@@ -106,7 +106,7 @@ CREATE TABLE "favourites" (
 );
 
 -- Contains search index
-CREATE TABLE "search_index" (
+CREATE TABLE IF NOT EXISTS "search_index" (
 	"id" INTEGER PRIMARY KEY NOT NULL,
 	"file_path" TEXT NOT NULL,
 	"keyword" TEXT NOT NULL, -- lower-cased keyword
@@ -116,11 +116,11 @@ CREATE TABLE "search_index" (
 	"last_indexed" INTEGER NOT NULL DEFAULT (STRFTIME('%s', 'now'))
 );
 -- Index everything (bad for updating the index, very good for reading the index)
-CREATE INDEX "search_index_file_path" ON "search_index"("file_path");
-CREATE INDEX "search_index_keyword" ON "search_index"("keyword");
-CREATE INDEX "search_index_type" ON "search_index"("type");
-CREATE INDEX "search_index_mtime" ON "search_index"("mtime");
-CREATE INDEX "search_index_size" ON "search_index"("size");
-CREATE INDEX "search_index_last_indexed" ON "search_index"("last_indexed");
+CREATE INDEX IF NOT EXISTS "search_index_file_path" ON "search_index"("file_path");
+CREATE INDEX IF NOT EXISTS "search_index_keyword" ON "search_index"("keyword");
+CREATE INDEX IF NOT EXISTS "search_index_type" ON "search_index"("type");
+CREATE INDEX IF NOT EXISTS "search_index_mtime" ON "search_index"("mtime");
+CREATE INDEX IF NOT EXISTS "search_index_size" ON "search_index"("size");
+CREATE INDEX IF NOT EXISTS "search_index_last_indexed" ON "search_index"("last_indexed");
 
 COMMIT TRANSACTION;
