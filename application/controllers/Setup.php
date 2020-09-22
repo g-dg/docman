a0b1e2158bd0616a41dd7c05c162fa01228c21a8
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-22 12:50:32 -0400

    Don't create the default admin user if there is already an admin user

--- application/controllers/Setup.php
+++ application/controllers/Setup.php
@@ -14,9 +14,20 @@ class Setup extends CI_Controller {
 		$default_username = $this->config->item('docman_default_username');
 		$default_password = $this->config->item('docman_default_password');
 
+		// we can attempt to recreate the database, even if it's already set up. This can act as a sort of recovery function.
 		$create_db_sql = file('./resources/database_schema.min.sql');
 		foreach ($create_db_sql as $sql) {
-			$this->db->simple_query($sql);
+			if (trim($sql) !== '') {
+				$this->db->simple_query($sql);
+			}
 		}
+
+		// only create the default admin user if there is no other admin user (in case all the admin accounts somehow get deleted)
+		$query = $this->db->query('SELECT COUNT() AS "count" FROM "users" WHERE "type" = 0;');
+		$row = $query->row_array();
+		if ($row['count'] == 0) {
+			$this->db->query('INSERT INTO "users" ("username", "password", "type", "settings") VALUES (?, ?, 0, \'{}\');', [$default_username, password_hash($default_password, PASSWORD_DEFAULT)]);
+		}
+
 	}
 }
