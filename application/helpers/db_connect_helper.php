5b5bae2b2d173b8bda961bad5e34fcd36a92aa5c
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-30 17:06:09 -0400

    Automatically get CI instance in db_connect helper

--- application/helpers/db_connect_helper.php
+++ application/helpers/db_connect_helper.php
@@ -5,20 +5,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * Connects to the database and runs some setup SQL statements
  * @param this_obj The CodeIgniter Object
  */
-function db_connect($this_obj)
+function db_connect()
 {
+	$CI =& get_instance();
 	static $connected = false;
 
 	if (!$connected) {
-		$this_obj->load->database();
+		$CI->load->database();
 
-		$this_obj->db->simple_query('PRAGMA journal_mode=WAL;');
-		$this_obj->db->simple_query('PRAGMA synchronous=NORMAL;');
+		$CI->db->simple_query('PRAGMA journal_mode=WAL;');
+		$CI->db->simple_query('PRAGMA synchronous=NORMAL;');
 
-		//$this_obj->db->simple_query('PRAGMA temp_store=MEMORY;');
-		//$this_obj->db->simple_query('PRAGMA cache_size=-16384');
+		//$CI->db->simple_query('PRAGMA temp_store=MEMORY;');
+		//$CI->db->simple_query('PRAGMA cache_size=-16384');
 
-		$this_obj->db->simple_query('PRAGMA foreign_keys = ON;');
+		$CI->db->simple_query('PRAGMA foreign_keys = ON;');
 
 		$connected = true;
 	}
