4b7c41014523187c80e50e7b58a76481e8c308a2
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-30 19:20:43 -0400

    Add Authentication::require_login

--- application/libraries/Authentication.php
+++ application/libraries/Authentication.php
@@ -1,13 +1,39 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Authentication {
+class Authentication
+{
 
 	private $CI;
 
 	public function __construct()
 	{
-		$this->CI =& get_instance();
+		$this->CI = &get_instance();
+	}
+
+	/**
+	 * Run to check if logged in, redirects to log in screen if not
+	 * @param target_url The URL (within the application) to redirect to after login
+	 */
+	public function require_login($target_url = null)
+	{
+		setup_session();
+
+		if (isset($_SESSION['docman_login_id'])) {
+			// check if login is valid (i.e. if the user has not logged out of all other locations)
+			if ((int)($this->CI->db->query('SELECT COUNT() AS "count" FROM "logins" WHERE "login_id" = ?;')->row_array()['count']) == 1) {
+				// update "last_used" of login table
+				$time = time();
+				// we don't want to lock the database for writing every second for each login
+				$this->CI->db->query('UPDATE "logins" SET "last_used" = ? WHERE "id" = ? AND "last_used" != ?;', [$time, $_SESSION['docman_login_id'], $time]);
+				return true;
+			}
+		}
+		$_SESSION['docman_login_redirect'] = is_null($target_url) ? $_SERVER['REQUEST_URI'] : rtrim($this->config->site_url(), '/') . $target_url;
+
+		$this->CI->load->helper('url');
+		redirect(html_escape(rtrim($this->config->site_url(), '/')) . '/login/', 'location', 303);
+		return false;
 	}
 
 	/**
@@ -18,21 +44,23 @@ class Authentication {
 	 */
 	public function login($username, $password)
 	{
-		$this->CI->load->library('session');
+		setup_session();
 		$auth_result = $this->authenticate($username, $password);
 
 		// check if successful
 		if (!is_null($auth_result)) {
+			$time = time();
 			// create login entry
 			$this->CI->db->query('INSERT INTO "logins" ("user_id", "client_addr", "user_agent", "login_time", "last_used") VALUES (?, ?, ?, ?, ?);', [
 				$auth_result,
 				isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : null,
 				isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : null,
-				time(),
-				time()
+				$time,
+				$time
 			]);
 			// set session
 			$_SESSION['docman_login_id'] = $this->CI->db->insert_id();
+			log_message('info', 'User "' . $username . '" logged in.');
 			return true;
 		}
 		return false;
@@ -57,13 +85,16 @@ class Authentication {
 					if (password_needs_rehash($user['password'], PASSWORD_DEFAULT)) {
 						// rehash and store
 						$this->CI->db->query('UPDATE "users" SET "password" = ? WHERE "id" = ?;', [password_hash($password, PASSWORD_DEFAULT), $user['id']]);
+						log_message('info', 'Rehashed password for "' . $username . '".');
 					}
 					return (int)$user['id'];
 				} else {
+					log_message('info', 'Attempted to log in as "' . $username . '" with incorrect password.');
 					return null;
 				}
 			}
 		}
+		log_message('info', 'Attempted to log in as non-existent user "' . $username . '".');
 		return null;
 	}
 }
