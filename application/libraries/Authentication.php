cc1db7cd615b8963d4019210b79437a26f1fa84c
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-30 08:25:25 -0400

    Add authentication function

--- application/libraries/Authentication.php
+++ application/libraries/Authentication.php
@@ -8,7 +8,6 @@ class Authentication {
 	const AUTH_SUCCESS = 1;
 	const AUTH_BAD_USERNAME = 2;
 	const AUTH_BAD_PASSWORD = 4;
-	const AUTH_NEED_PASSWORD_CHANGE = 8;
 
 	public function __construct()
 	{
@@ -16,11 +15,40 @@ class Authentication {
 	}
 
 	/**
-	 * 
+	 * Authenticates and logs in a user
+	 */
+	public function login($username, $password)
+	{
+		$this->CI->load->library('session');
+		$auth_result = $this->authenticate($username, $password);
+	}
+
+	/**
+	 * Ensures a user's credentials are valid
+	 * @param username The username to check
+	 * @param password The password to check
+	 * @return int Bitmask describing authentication result
 	 */
 	public function authenticate($username, $password)
 	{
-
+		// get user from database
+		$query = $this->CI->db->query('SELECT "id", "username", "password", "type", "full_name", "last_password_change", "settings" FROM "users" WHERE "username" = ?;', [$username]);
+		foreach ($query->result_array() as $user) {
+			// check username
+			if ($user['username'] == $username) {
+				// check password
+				if (password_verify($password, $user['password'])) {
+					// check whether password needs rehashing
+					if (password_needs_rehash($user['password'], PASSWORD_DEFAULT)) {
+						// rehash and store
+						$this->CI->db->query('UPDATE "users" SET "password" = ? WHERE "id" = ?;', [password_hash($password, PASSWORD_DEFAULT), $user['id']]);
+					}
+					return self::AUTH_SUCCESS;
+				} else {
+					return self::AUTH_BAD_PASSWORD;
+				}
+			}
+		}
+		return self::AUTH_BAD_USERNAME;
 	}
-
 }
