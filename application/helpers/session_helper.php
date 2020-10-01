8d468bb950cfe9a6d519a1a1b50571bcec01097a
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-09-30 21:29:44 -0400

    Format session helper

--- application/helpers/session_helper.php
+++ application/helpers/session_helper.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /**
  * Connects to the database and runs some setup SQL statements
@@ -7,8 +7,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 function setup_session()
 {
-	$CI =& get_instance();
+	$CI = &get_instance();
 	$CI->load->library('session');
+
+	/*if (session_status() == PHP_SESSION_NONE) {
+		session_start();
+	}*/
 
 	if (!isset($_SESSION['docman_csrf_token'])) {
 		$_SESSION['docman_csrf_token'] = hash('sha512', $CI->security->get_random_bytes(4096));
@@ -21,7 +25,8 @@ function setup_session()
  * @param csrf_token The token to check against
  * @return bool Whether the check passed
  */
-function check_csrf_token($csrf_token) {
+function check_csrf_token($csrf_token)
+{
 	setup_session();
 	return ($csrf_token === $_SESSION['docman_csrf_token']);
 }
@@ -30,7 +35,8 @@ function check_csrf_token($csrf_token) {
  * Gets the CSRF token
  * @return string the CSRF token
  */
-function get_csrf_token() {
+function get_csrf_token()
+{
 	setup_session();
 	return $_SESSION['docman_csrf_token'];
 }
