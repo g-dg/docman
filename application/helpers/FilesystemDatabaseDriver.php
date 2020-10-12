d1f63d49b34e1f51e4da6ff299945c37105f25dc
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-10-12 16:34:36 -0400

    Add various filesystem drivers

---
+++ application/helpers/FilesystemDatabaseDriver.php
@@ -0,0 +1,201 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class FilesystemDatabaseDirectoryHandle implements IFilesystemDirectoryHandle
+{
+	private $driver;
+	private $CI;
+	public function __construct($driver)
+	{
+		$this->driver = $driver;
+		$this->CI = &get_instance();
+	}
+
+	public function opendir($path)
+	{
+	}
+
+	public function closedir()
+	{
+	}
+
+	public function readdir()
+	{
+	}
+
+	public function rewinddir()
+	{
+	}
+}
+
+class FilesystemDatabaseFileHandle implements IFilesystemFileHandle
+{
+	private $driver;
+	private $CI;
+	public function __construct($driver)
+	{
+		$this->driver = $driver;
+		$this->CI = &get_instance();
+	}
+
+	public function fopen($path, $mode)
+	{
+	}
+
+	public function fclose()
+	{
+	}
+
+	public function fread($length)
+	{
+	}
+	public function fwrite($string, $length = null)
+	{
+	}
+	public function feof()
+	{
+	}
+	public function ftell()
+	{
+	}
+	public function fseek($offset)
+	{
+	}
+	public function rewind()
+	{
+	}
+}
+
+class FilesystemDatabaseDriver implements IFilesystemDriver
+{
+	private $CI;
+	public $mountpoint_id;
+	public function __construct($driver_name, $driver_options, $mountpoint_id)
+	{
+		$this->CI = &get_instance();
+		$this->mountpoint_id = $mountpoint_id;
+	}
+
+	public function mount()
+	{
+	}
+	public function unmount()
+	{
+	}
+
+	public function opendir($path)
+	{
+	}
+	public function closedir($directory_handle)
+	{
+	}
+	public function readdir($directory_handle)
+	{
+	}
+	public function rewinddir($directory_handle)
+	{
+	}
+
+	public function fopen($path, $mode)
+	{
+	}
+	public function fclose($file_handle)
+	{
+	}
+	public function fread($file_handle, $length)
+	{
+	}
+	public function fwrite($file_handle, $string, $length = null)
+	{
+	}
+	public function feof($file_handle)
+	{
+	}
+	public function ftell($file_handle)
+	{
+	}
+	public function fseek($file_handle, $offset)
+	{
+	}
+	public function rewind($file_handle)
+	{
+	}
+
+	public function disk_usage($directory)
+	{
+	}
+	public function file_exists($filename)
+	{
+	}
+	public function filemtime($filename)
+	{
+	}
+	public function filesize($filename)
+	{
+	}
+	public function filetype($filename)
+	{
+	}
+	public function filecount($directory)
+	{
+	}
+	public function is_readable($filename)
+	{
+	}
+	public function is_writable($filename)
+	{
+	}
+	public function touch($filename)
+	{
+	}
+	public function mkdir($pathname)
+	{
+	}
+	public function rmdir($pathname)
+	{
+	}
+	public function unlink($filename)
+	{
+	}
+	public function copy($source, $destination)
+	{
+	}
+	public function move($source, $destination)
+	{
+	}
+
+	public function get_display_name($filename)
+	{
+	}
+	public function set_display_name($filename, $display_name)
+	{
+	}
+
+	public function get_access_link($filename)
+	{
+	}
+
+	public function get_owner_id($filename)
+	{
+	}
+	public function set_owner_id($filename, $owner_id)
+	{
+	}
+
+	public function get_permissions($filename, $group_ids = null)
+	{
+	}
+	public function set_permissions($filename, $permissions)
+	{
+	}
+
+	public function get_tags($filename)
+	{
+	}
+	public function add_tag($filename, $tag_id)
+	{
+	}
+	public function remove_tag($filename, $tag_id)
+	{
+	}
+}
---
+++ application/helpers/FilesystemDummyDriver.php
@@ -0,0 +1,236 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+
+class FilesystemDummyDirectoryHandle implements IFilesystemDirectoryHandle
+{
+	public function __construct($driver)
+	{
+	}
+
+	public function opendir($path)
+	{
+		return false;
+	}
+
+	public function closedir()
+	{
+		return false;
+	}
+
+	public function readdir()
+	{
+		return false;
+	}
+
+	public function rewinddir()
+	{
+		return false;
+	}
+}
+
+class FilesystemDummyFileHandle implements IFilesystemFileHandle
+{
+	public function __construct($driver)
+	{
+	}
+
+	public function fopen($path, $mode)
+	{
+		return false;
+	}
+
+	public function fclose()
+	{
+		return false;
+	}
+
+	public function fread($length)
+	{
+		return false;
+	}
+	public function fwrite($string, $length = null)
+	{
+		return false;
+	}
+	public function feof()
+	{
+		return true;
+	}
+	public function ftell()
+	{
+		return false;
+	}
+	public function fseek($offset)
+	{
+		return -1;
+	}
+	public function rewind()
+	{
+		return false;
+	}
+}
+
+class FilesystemDummyDriver implements IFilesystemDriver
+{
+	private $CI;
+	public $mountpoint_id;
+	public function __construct($driver_name, $driver_options, $mountpoint_id)
+	{
+		$this->CI = &get_instance();
+		$this->mountpoint_id = $mountpoint_id;
+	}
+
+	public function mount()
+	{
+		return true;
+	}
+	public function unmount()
+	{
+		return true;
+	}
+
+	public function opendir($path)
+	{
+		$dh = new FilesystemDummyDirectoryHandle($this);
+		return $dh->opendir($path);
+	}
+	public function closedir($directory_handle)
+	{
+		return $directory_handle->closedir();
+	}
+	public function readdir($directory_handle)
+	{
+		return $directory_handle->readdir();
+	}
+	public function rewinddir($directory_handle)
+	{
+		return $directory_handle->rewinddir();
+	}
+
+	public function fopen($path, $mode)
+	{
+		$fh = new FilesystemDummyFileHandle($this);
+		return $fh->fopen($path, $mode);
+	}
+	public function fclose($file_handle)
+	{
+		return $file_handle->fclose();
+	}
+	public function fread($file_handle, $length)
+	{
+		return $file_handle->fread($length);
+	}
+	public function fwrite($file_handle, $string, $length = null)
+	{
+		return $file_handle->fwrite($string, $length);
+	}
+	public function feof($file_handle)
+	{
+		return $file_handle->feof();
+	}
+	public function ftell($file_handle)
+	{
+		return $file_handle->ftell();
+	}
+	public function fseek($file_handle, $offset)
+	{
+		return $file_handle->fseek($offset);
+	}
+	public function rewind($file_handle)
+	{
+		return $file_handle->rewind();
+	}
+
+	public function disk_usage($directory)
+	{
+		return ['free' => 0, 'used' => 0, 'total' => 0];
+	}
+	public function file_exists($filename)
+	{
+		return true;
+	}
+	public function filemtime($filename)
+	{
+		return time();
+	}
+	public function filesize($filename)
+	{
+		return 0;
+	}
+	public function filetype($filename)
+	{
+		return 'unknown';
+	}
+	public function filecount($directory)
+	{
+		return 0;
+	}
+	public function is_readable($filename)
+	{
+		return false;
+	}
+	public function is_writable($filename)
+	{
+		return false;
+	}
+	public function touch($filename)
+	{
+		return false;
+	}
+	public function mkdir($pathname)
+	{
+		return false;
+	}
+	public function rmdir($pathname)
+	{
+		return false;
+	}
+	public function unlink($filename)
+	{
+		return false;
+	}
+	public function copy($source, $destination)
+	{
+		return false;
+	}
+	public function move($source, $destination)
+	{
+		return false;
+	}
+
+	public function get_display_name($filename)
+	{
+	}
+	public function set_display_name($filename, $display_name)
+	{
+	}
+
+	public function get_access_link($filename)
+	{
+		return null;
+	}
+
+	public function get_owner_id($filename)
+	{
+	}
+	public function set_owner_id($filename, $owner_id)
+	{
+	}
+
+	public function get_permissions($filename, $group_ids = null)
+	{
+	}
+	public function set_permissions($filename, $permissions)
+	{
+	}
+
+	public function get_tags($filename)
+	{
+	}
+	public function add_tag($filename, $tag_id)
+	{
+	}
+	public function remove_tag($filename, $tag_id)
+	{
+	}
+}
