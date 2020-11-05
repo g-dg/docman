7957642ecdcc78535eaad4c7b0fcd232d2af568e
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-11-05 17:20:58 -0500

    Add header and footer views

---
+++ application/views/template/header.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title><?=htmlspecialchars($title);?> - Garnet DeGelder's DocMan <?=DOCMAN_VERSION?></title>
+</head>
+<body>
