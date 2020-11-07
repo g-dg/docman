69fefa2fea6b38ab6581a060183c302bed8c7877
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-11-06 21:28:19 -0500

    Fix typo in footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,7 +1,7 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 	</main>
 	<footer>
-		Docman v<?= htmlspecialchars(DOCMAN_VERSION); ?> <?= htmlspecialchars(DOCMAN_COPYRIGHT); ?>
+		DocMan v<?= htmlspecialchars(DOCMAN_VERSION); ?> <?= htmlspecialchars(DOCMAN_COPYRIGHT); ?>
 	</footer>
 </body>
 
