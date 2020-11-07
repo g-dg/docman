1d263b3dd04c89e0bf17c8761688d4e2a8a453d4
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-11-06 19:55:08 -0500

    Autoload url helper

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('db_connect', 'session');
+$autoload['helper'] = array('db_connect', 'session', 'url');
 
 /*
 | -------------------------------------------------------------------
