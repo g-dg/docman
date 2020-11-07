4f0960351f2dfc54b58978f71159de9863d0a26b
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-11-06 20:09:33 -0500

    Add empty browse view

---
+++ application/views/browse.php
@@ -0,0 +1,4 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<?php $this->load->view('template/header', ['title' => 'Browse']); ?>
+
+<?php $this->load->view('template/footer'); ?>
