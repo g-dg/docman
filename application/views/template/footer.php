e86d735ea0acffdf0825b47952e4d48d158b9bf1
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-11-06 20:08:28 -0500

    Add content to header and footer

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -1,4 +1,8 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-?></body>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+	</main>
+	<footer>
+		Docman v<?= htmlspecialchars(DOCMAN_VERSION); ?> <?= htmlspecialchars(DOCMAN_COPYRIGHT); ?>
+	</footer>
+</body>
+
 </html>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,10 +1,25 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
+
+$this->load->library('authentication');
+
 ?><!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title><?=htmlspecialchars($title);?> - Garnet DeGelder's DocMan <?=DOCMAN_VERSION?></title>
+	<title><?= htmlspecialchars($title); ?> - Garnet DeGelder's DocMan <?= DOCMAN_VERSION ?></title>
+	<link rel="stylesheet" src="<?= html_escape($this->config->base_url('/resources/css/main.css')) ?>" />
+	<link rel="stylesheet" src="<?= html_escape($this->config->base_url('/resources/css/template.css')) ?>" />
 </head>
+
 <body>
+	<header>
+		<ul>
+			<li><a href="<?= html_escape($this->config->site_url('/browse')) ?>">Browse</a></li>
+			<li>Logged in as <?= htmlspecialchars($this->authentication->get_current_username()) ?></li>
+			<li><a href="<?= html_escape($this->config->site_url('/login/logout')) ?>">Log out</a></li>
+		</ul>
+	</header>
+	<main>
