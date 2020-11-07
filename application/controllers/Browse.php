0dc80a6ab75dcd7a213c08e15a2617bbafab5ef9
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-11-06 22:28:01 -0500

    Add basic file browsing

--- application/controllers/Browse.php
+++ application/controllers/Browse.php
@@ -19,5 +19,91 @@ class Browse extends CI_Controller
 		}
 
 		$this->authentication->require_login();
+
+		$path = '/' . implode('/', $path_array);
+
+		$this->load->library('filesystem');
+
+		// check if it's a file
+		if ($this->filesystem->filetype($path) != 'dir') {
+			redirect(site_url('/file/open' . $this->url_encode_path($path)));
+			return;
+		}
+
+		// get file information
+		$files = [];
+		$dh = $this->filesystem->opendir($path);
+		if (!$dh) {
+			show_404();
+			return;
+		}
+
+		$files = [];
+		if (rtrim($path, '/') !== '') {
+			$files[] = [
+				'name' => '..',
+				'realname' => '..',
+				'url' => site_url('/browse' . dirname($path)),
+				'type' => 'dir',
+				'size' => $this->filesystem->filecount(dirname($path)),
+				'mtime' => $this->filesystem->filemtime(dirname($path))
+			];
+		}
+
+		while (($file = $this->filesystem->readdir($dh)) !== false) {
+			if ($file !== '.' && $file !== '..') {
+
+				$filepath = rtrim($path, '/') . '/' . $file;
+
+				$displayname = $this->filesystem->get_display_name($filepath);
+
+				$filetype = $this->filesystem->filetype($filepath);
+
+				switch ($filetype) {
+					case 'dir':
+						$displayname .= '/';
+						$url = site_url('/browse' . $filepath);
+						$size = $this->filesystem->filecount($filepath);
+						break;
+					case 'file':
+						$url = site_url('/file' . $filepath);
+						$size = $this->filesystem->filesize($filepath);
+						break;
+					default:
+						$url = site_url('/browse' . $path);
+						$size = null;
+						break;
+				}
+
+				$mtime = $this->filesystem->filemtime($filepath);
+
+				$files[] = [
+					'name' => $displayname,
+					'realname' => $file,
+					'url' => $url,
+					'type' => $filetype,
+					'size' => $size,
+					'mtime' => $mtime
+				];
+			}
+		}
+
+		$this->filesystem->closedir($dh);
+
+		usort($files, function ($a, $b) {
+			return strcmp($a['name'], $b['name']);
+		});
+
+		$this->load->view('browse', ['files' => $files]);
+	}
+
+	private function url_encode_path($path)
+	{
+		$path_array = explode('/', trim($path, '/'));
+		$new_path_array = [];
+		foreach ($path_array as $path_part) {
+			$new_path_array[] = rawurlencode($path_part);
+		}
+		return '/' . implode('/', $new_path_array);
 	}
 }
--- application/views/browse.php
+++ application/views/browse.php
@@ -1,4 +1,25 @@
 <?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
 <?php $this->load->view('template/header', ['title' => 'Browse']); ?>
 
+<table>
+	<thead>
+		<tr>
+			<th>Type</th>
+			<th>Name</th>
+			<th>Size</th>
+			<th>Last Modified</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php foreach ($files as $file) { ?>
+			<tr>
+				<td><?= htmlspecialchars($file['type']) ?></td>
+				<td><a href="<?= htmlspecialchars($file['url']) ?>"><?= htmlspecialchars($file['name']) ?></a></td>
+				<td><?= htmlspecialchars($file['size']) ?></td>
+				<td><?= htmlspecialchars(date('c', $file['mtime'])) ?></td>
+			</tr>
+		<?php } ?>
+	</tbody>
+</table>
+
 <?php $this->load->view('template/footer'); ?>
