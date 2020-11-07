951f4c28b0ba657f7b5167bddec6928ae64e50ae
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-11-06 20:19:46 -0500

    Improve header view

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -10,16 +10,16 @@ $this->load->library('authentication');
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title><?= htmlspecialchars($title); ?> - Garnet DeGelder's DocMan <?= DOCMAN_VERSION ?></title>
-	<link rel="stylesheet" src="<?= html_escape($this->config->base_url('/resources/css/main.css')) ?>" />
-	<link rel="stylesheet" src="<?= html_escape($this->config->base_url('/resources/css/template.css')) ?>" />
+	<link rel="stylesheet" src="<?= html_escape(base_url('/resources/css/main.css')) ?>" />
+	<link rel="stylesheet" src="<?= html_escape(base_url('/resources/css/template.css')) ?>" />
 </head>
 
 <body>
 	<header>
 		<ul>
-			<li><a href="<?= html_escape($this->config->site_url('/browse')) ?>">Browse</a></li>
+			<li><a href="<?= html_escape(site_url('/browse')) ?>">Browse</a></li>
 			<li>Logged in as <?= htmlspecialchars($this->authentication->get_current_username()) ?></li>
-			<li><a href="<?= html_escape($this->config->site_url('/login/logout')) ?>">Log out</a></li>
+			<li><a href="<?= html_escape(site_url('/login/logout')) ?>">Log out</a></li>
 		</ul>
 	</header>
 	<main>
