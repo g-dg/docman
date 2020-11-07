0040620e6a824d9efb6d6147d007546126718f3a
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2020-11-06 20:09:10 -0500

    Add login css file

--- application/views/login.php
+++ application/views/login.php
@@ -1,14 +1,19 @@
-<?php defined('BASEPATH') or exit('No direct script access allowed'); ?><!DOCTYPE html>
+<?php defined('BASEPATH') or exit('No direct script access allowed'); ?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Log In - Garnet DeGelder's DocMan <?=DOCMAN_VERSION?></title>
+	<title>Log In - Garnet DeGelder's DocMan <?= DOCMAN_VERSION ?></title>
+	<link rel="stylesheet" src="<?= html_escape($this->config->base_url('/resources/css/main.css')) ?>" />
+	<link rel="stylesheet" src="<?= html_escape($this->config->base_url('/resources/css/login.css')) ?>" />
 </head>
+
 <body>
-	<form action="<?= rtrim(html_escape($this->config->site_url()), '/') ?>/login/do_login" method="POST">
-		<input name="_csrf_token" value="<?= html_escape(get_csrf_token()) ?>" type="hidden" />
-		<h1>Log In - Garnet DeGelder's DocMan <?=DOCMAN_VERSION?></h1>
+	<form action="<?= html_escape($this->config->site_url('/login/do_login')); ?>" method="POST">
+		<input name="_csrf_token" value="<?= html_escape(get_csrf_token()); ?>" type="hidden" />
+		<h1>Log In - Garnet DeGelder's DocMan <?= DOCMAN_VERSION ?></h1>
 		<label for="username">Username:</label>
 		<input id="username" name="username" type="text" />
 		<br />
@@ -16,7 +21,8 @@
 		<input id="password" name="password" type="password" />
 		<br />
 		<input type="submit" value="Log In" />
-		<?= isset($_SESSION['docman_login_result']) ? html_escape($_SESSION['docman_login_result']) : '' ?>
+		<?= isset($_SESSION['docman_login_result']) ? html_escape($_SESSION['docman_login_result']) : ''; ?>
 	</form>
 </body>
+
 </html>
