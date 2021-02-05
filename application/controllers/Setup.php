017e4689d635e709c2d37272ff3882f7022bb8c5
Garnet DeGelder <garnet.h.degelder17@gmail.com>
2021-02-04 19:52:42 -0500

    Improve setup to automatically create the default mountpoint

--- application/controllers/Setup.php
+++ application/controllers/Setup.php
@@ -27,7 +27,37 @@ class Setup extends CI_Controller {
 		$row = $query->row_array();
 		if ($row['count'] == 0) {
 			$this->db->query('INSERT INTO "users" ("username", "password", "type", "settings") VALUES (?, ?, 0, \'{}\');', [$default_username, password_hash($default_password, PASSWORD_DEFAULT)]);
+			$user_id = $this->db->insert_id();
+
+			$this->db->query('INSERT INTO "groups" ("name") VALUES (?);', [$default_username]);
+			$group_id = $this->db->insert_id();
+
+			$this->db->query('INSERT INTO "users_in_groups" ("user_id", "group_id") VALUES (?, ?);', [$user_id, $group_id]);
+
+			// check if we need to create a mountpoint in case all mountpoints also got deleted
+			if ((int)($this->db->query('SELECT COUNT() AS "count" FROM "mountpoints";')->result_array()[0]['count']) === 0) {
+				$this->db->query('INSERT INTO "mountpoints" ("destination_path", "driver", "driver_options") VALUES (\'/\', \'server_fs\', ?);', [
+					json_encode([
+						'server_fs' => [
+							'path' => $this->config->item('docman_default_data_directory')
+						]
+					])
+				]);
+				$mountpoint_id = $this->db->insert_id();
+
+				$this->db->query('INSERT INTO "files" ("mountpoint_id", "path_in_mountpoint", "display_name", "owner_user_id", "mountpoint_driver_info") VALUES (?, \'/\', NULL, ?, \'{}\')', [
+					$mountpoint_id,
+					$user_id,
+				]);
+				$file_id = $this->db->insert_id();
+
+				$this->db->query('INSERT INTO "file_permissions" ("file_id", "group_id", "read", "write", "share", "expires") VALUES (?, ?, 1, 1, 1, NULL);', [
+					$file_id,
+					$group_id
+				]);
+			}
 		}
 
+		redirect(site_url(), 'location', 303);
 	}
 }
